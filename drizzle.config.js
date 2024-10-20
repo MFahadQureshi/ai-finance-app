30974c2a3a6aa81fef55f2cba167bb8667c6a0c0
Muhammad Fahad Qureshi <155016268+MFahadQureshi@users.noreply.github.com>
2024-10-21 02:36:12 +0500

    making the finance app

---
+++ drizzle.config.js
@@ -0,0 +1,9 @@
+export default {
+  dialect: "postgresql",
+  schema: "./utils/schema.jsx",
+  out: "./drizzle",
+  dbCredentials: {
+    url: 'postgresql://finance-ap_owner:1jpxknZq8rDL@ep-shy-salad-a57j06ke.us-east-2.aws.neon.tech/finance-ap?sslmode=require',
+    connectionString: 'postgresql://finance-ap_owner:1jpxknZq8rDL@ep-shy-salad-a57j06ke.us-east-2.aws.neon.tech/finance-ap?sslmode=require'
+  }
+};
