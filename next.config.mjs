df51e4f6ff330d43c93b61ded3ef15186d045d6a
Muhammad Fahad Qureshi <155016268+MFahadQureshi@users.noreply.github.com>
2024-10-22 04:53:40 +0500

    next js config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+module.exports = {
+    cacheHandler: require.resolve('./cache-handler.js'),
+    cacheMaxMemorySize: 0, // disable default in-memory caching
+  }
+
 export default nextConfig;
